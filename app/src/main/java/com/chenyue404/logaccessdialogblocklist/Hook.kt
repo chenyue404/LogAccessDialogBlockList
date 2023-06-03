8fcbd1a318aafa10e40afc35a2c5cf7724683a42
chenyue404 <601450225@qq.com>
2023-06-03 14:25:21 +0800

    first commit

---
+++ app/src/main/java/com/chenyue404/logaccessdialogblocklist/Hook.kt
@@ -0,0 +1,116 @@
+package com.chenyue404.logaccessdialogblocklist
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Handler
+import android.widget.Toast
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XSharedPreferences
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers.callMethod
+import de.robv.android.xposed.XposedHelpers.findAndHookMethod
+import de.robv.android.xposed.XposedHelpers.findClassIfExists
+import de.robv.android.xposed.XposedHelpers.getObjectField
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+/**
+ * Created by chenyue on 2023/4/16 0016.
+ */
+class Hook : IXposedHookLoadPackage {
+    companion object {
+        const val PREF_NAME = "android"
+        const val KEY = "key"
+        val pref: SharedPreferences? by lazy {
+            val pref = XSharedPreferences(BuildConfig.APPLICATION_ID, PREF_NAME)
+            if (pref.file.canRead()) pref else null
+        }
+    }
+
+    private val TAG = "LogAccessDialogActivity-hook-"
+
+    private fun log(str: String) {
+        XposedBridge.log("$TAG$str")
+    }
+
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        val packageName = lpparam.packageName
+        val classLoader = lpparam.classLoader
+
+//        val clazz =
+//            findClassIfExists("com.android.server.logcat.LogAccessDialogActivity", classLoader)
+//        log("clazz exit: ${clazz != null}, packageName=$packageName")
+//        try {
+//            findMethodBestMatch(
+//                clazz,
+//                "readIntentInfo",
+//                Intent::class.java,
+//                object : XC_MethodHook() {
+//                    override fun afterHookedMethod(param: MethodHookParam) {
+//                        val pkgName = getObjectField(param.thisObject, "mPackageName") as String?
+//                        log("readIntentInfo: pkg=$pkgName")
+//                        if (param.result == false || TextUtils.isEmpty(pkgName)) {
+//                            return
+//                        }
+//                        if (pref?.getStringSet(KEY, setOf())?.any {
+//                                pkgName!!.contains(it, true)
+//                            } == true) {
+//                            log("block: $pkgName")
+//                            param.result = false
+//                        }
+//                    }
+//                })
+//        } catch (e: NullPointerException) {
+//            e.printStackTrace()
+//        } catch (e: NoSuchMethodException) {
+//            e.printStackTrace()
+//        } catch (e: NoClassDefFoundError) {
+//            e.printStackTrace()
+//        }
+
+        val clazzLogAccessClient = findClassIfExists(
+            "com.android.server.logcat.LogcatManagerService.LogAccessClient",
+            classLoader
+        )
+        try {
+            findAndHookMethod("com.android.server.logcat.LogcatManagerService", classLoader,
+                "processNewLogAccessRequest",
+                clazzLogAccessClient,
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        val logAccessClient = param.args.first()
+                        val mPackageName = getObjectField(logAccessClient, "mPackageName") as String
+                        log("mPackageName=$mPackageName")
+
+                        val stringSet = pref?.getStringSet(KEY, setOf())
+                        log(stringSet?.joinToString().toString())
+                        if (stringSet?.any {
+                                mPackageName.contains(it, true)
+                            } == true) {
+                            log("block: $mPackageName")
+                            val mContext = (getObjectField(
+                                param.thisObject,
+                                "mContext"
+                            ) as Context).applicationContext
+                            Handler(mContext.mainLooper).post {
+                                Toast.makeText(
+                                    mContext,
+                                    "Blocked LogAccessRequest of $mPackageName",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            callMethod(
+                                param.thisObject,
+                                "onAccessDeclinedForClient",
+                                logAccessClient
+                            )
+                            param.result = null
+                        }
+                    }
+                }
+            )
+        } catch (e: Throwable) {
+            e.printStackTrace()
+        }
+    }
+}
---
+++ app/src/main/java/com/chenyue404/logaccessdialogblocklist/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.chenyue404.logaccessdialogblocklist
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.content.edit
+
+class MainActivity : Activity() {
+
+    private val etContent: EditText by lazy { findViewById(R.id.etContent) }
+    private val btSave: Button by lazy { findViewById(R.id.btSave) }
+    private val sp by lazy {
+        try {
+            getSharedPreferences(
+                Hook.PREF_NAME,
+                Context.MODE_WORLD_READABLE
+            )
+        } catch (e: SecurityException) {
+            // The new XSharedPreferences is not enabled or module's not loading
+            null // other fallback, if any
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etContent.setText(sp?.getStringSet(Hook.KEY, emptySet())?.joinToString())
+        btSave.setOnClickListener {
+            val set = etContent.text.toString()
+                .split(",")
+                .toMutableSet().onEach { it.trim() }
+            etContent.setText(set.joinToString())
+            sp?.edit {
+                putStringSet(Hook.KEY, set)
+            }
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
